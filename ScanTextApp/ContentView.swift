16d136c1f817aa252bc46943faa9877d01a5715d
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-05-04 13:56:27 -0400

    Initial Commit

---
+++ ScanTextApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ScanTextApp
+//
+//  Created by Enrique Sarmiento on 4/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ ScanTextApp/ScanTextAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanTextAppApp.swift
+//  ScanTextApp
+//
+//  Created by Enrique Sarmiento on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ScanTextAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
