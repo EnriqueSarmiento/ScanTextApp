8e8366fcbf3b4241566b1aae0820293e5be7a862
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-05-10 13:05:24 -0400

    feature: scan text with a qr code done

--- ScanTextApp/QR/QRView.swift
+++ ScanTextApp/QR/QRView.swift
@@ -6,9 +6,39 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct QRView: View {
+   
+   @State private var showScanner: Bool = false
+   @State private var qrtext: String = "Scanear QR"
+   
+   func scan(result: Result<ScanResult, ScanError>){
+      showScanner = false
+      switch result {
+      case .success(let result):
+         let details = result.string.components(separatedBy: "")
+         qrtext = details[0]
+      case .failure(let error):
+         print("DEBUG: fallo el scaneo", error.localizedDescription)
+     
+      }
+   }
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       NavigationView{
+          VStack(alignment: .center){
+             Button {
+                showScanner = true
+             } label: {
+                Text("Scanear")
+             }.sheet(isPresented: $showScanner) {
+                CodeScannerView(codeTypes: [.qr], completion: scan)
+             }
+             
+             Text(qrtext)
+
+          }.navigationTitle("Scanear QR")
+       }
     }
 }
